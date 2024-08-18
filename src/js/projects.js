0962a27c3336c8acfc605c37db9607876ed61a67
Anna Vezhdel <avezhdel@gmail.com>
2024-08-18 20:28:03 +0200

    update

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,16 +1,58 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
+
+import book_mob_1x from '../img/projects/book-mob.jpg';
+import book_mob_2x from '../img/projects/book-mob-2x.jpg';
+import book_tab_1x from '../img/projects/book-tab.jpg';
+import book_tab_2x from '../img/projects/book-tab-2x.jpg';
+import book_main from '../img/projects/book-tab.jpg';
+
+import pet_mob_1x from '../img/projects/pet-mob.jpg';
+import pet_mob_2x from '../img/projects/pet-mob-2x.jpg';
+import pet_tab_1x from '../img/projects/pet-tab.jpg';
+import pet_tab_2x from '../img/projects/pet-tab-2x.jpg';
+import pet_main from '../img/projects/pet-tab.jpg';
+
+import vocab_mob_1x from '../img/projects/vocab-mob.jpg';
+import vocab_mob_2x from '../img/projects/vocab-mob-2x.jpg';
+import vocab_tab_1x from '../img/projects/vocab-tab.jpg';
+import vocab_tab_2x from '../img/projects/vocab-tab-2x.jpg';
+import vocab_main from '../img/projects/vocab-tab.jpg';
+
+const projectImages = [
+{
+mob_1x: book_mob_1x,
+mob_2x: book_mob_2x,
+tab_1x: book_tab_1x,
+tab_2x: book_tab_2x,
+main: book_main,
+description: 'ReadJourney',
+},
+{
+mob_1x: pet_mob_1x,
+mob_2x: pet_mob_2x,
+tab_1x: pet_tab_1x,
+tab_2x: pet_tab_2x,
+main: pet_main,
+description: 'PetLove',
+},
+{
+mob_1x: vocab_mob_1x,
+mob_2x: vocab_mob_2x,
+tab_1x: vocab_tab_1x,
+tab_2x: vocab_tab_2x,
+main: vocab_main,
+description: 'VocabBuilder',
+},
+];
 
 new Swiper('.swiper-projects', {
-modules: [Navigation, Mousewheel, Keyboard],
 direction: 'horizontal',
 navigation: {
 nextEl: '.projects-btn-next',
 prevEl: '.projects-btn-prev',
 },
-
 slidesPerView: 1,
 spaceBetween: 34,
 keyboard: {
@@ -21,34 +63,14 @@ onlyInViewport: false,
 
 const projectsList = document.querySelector('.projects-list');
 
-const projectImages = [
-{
-mob_1x: '../img/projects/book-mob.jpg',
-mob_2x: '../img/projects/book-mob-2x.jpg',
-tab_1x: '../img/projects/book-tab.jpg',
-tab_2x: '../img/projects/book-tab-2x.jpg',
-main: '../img/projects/book-tab.jpg',
-description: 'ReadJourney',
-},
-{
-mob_1x: '../img/projects/pet-mob.jpg',
-mob_2x: '../img/projects/pet-mob-2x.jpg',
-tab_1x: '../img/projects/pet-tab.jpg',
-tab_2x: '../img/projects/pet-tab-2x.jpg',
-main: '../img/projects/pet-tab.jpg',
-description: 'PetLove',
-},
-{
-mob_1x: '../img/projects/vocab-mob.jpg',
-mob_2x: '../img/projects/vocab-mob-2x.jpg',
-tab_1x: '../img/projects/vocab-tab.jpg',
-tab_2x: '../img/projects/vocab-tab-2x.jpg',
-main: '../img/projects/vocab-tab.jpg',
-description: 'VocabBuilder',
-}
-];
-
-function createProjectsSlideMarkup(mob_1x, mob_2x, tab_1x, tab_2x, main, description) {
+function createProjectsSlideMarkup(
+mob_1x,
+mob_2x,
+tab_1x,
+tab_2x,
+main,
+description
+) {
 const markup = `
     <li class="swiper-slide projects-slide">
     <div class="projects-items">
@@ -103,6 +125,6 @@ const markup = `
 
 projectsList.insertAdjacentHTML('beforeend', markup);
 }
-projectImages.map(({ mob_1x, mob_2x, tab_1x, tab_2x, main,description }) => {
+projectImages.map(({ mob_1x, mob_2x, tab_1x, tab_2x, main, description }) => {
 createProjectsSlideMarkup(mob_1x, mob_2x, tab_1x, tab_2x, main, description);
 });
